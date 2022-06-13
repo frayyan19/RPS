b3121fcda93dcbbdf8e9d614ce01028a8d24b742
frayyan19 <frayyan@umich.edu>
2022-06-13 16:22:51 -0400

    working

--- rps.js
+++ rps.js
@@ -1,12 +1,35 @@
 
+
+let playScore = 5;
+let compScore = 5;
+let str = ""
+let playerSelection = ""
+const rockBtn = document.getElementById('rock')
+const paperBtn = document.getElementById('paper')
+const scissorsBtn = document.getElementById('scissors')
+
+
+function handleClick(rps){
+    playerSelection = rps
+    str =  playRound(playerSelection,computerPlay())
+    playerSelection = ""
+    console.log(str)
+    if(str[4] == 'W') --playScore
+    else if(str[4] == 'L') --compScore
+    const content = document.getElementById('score');
+    content.innerHTML = `P: ${playScore} Lives  - C: ${compScore} Lives`;
+    
+    if(compScore == 0) return alert("YOU WIN!")
+    if(playScore == 0) return alert("YOU LOSE!")
+}
+
 function computerPlay() {
     const arr = ["Rock","Paper","Scissors"]
-    let val = Math.floor(Math.random()*2)
+    let val = Math.floor(Math.random()*3)
     return arr[val]
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection[0].toUpperCase() + playerSelection.substing(1,playerSelection.length()).toLowerCase()
     const rs = playerSelection === 'Rock' && computerSelection === 'Scissors'
     const sp = playerSelection === 'Scissors' && computerSelection === 'Paper'
     const pr = playerSelection === 'Paper' && computerSelection === 'Rock'
@@ -14,7 +37,13 @@ function playRound(playerSelection, computerSelection) {
     if(winner){
             return "You Win! " + playerSelection + " beats " + computerSelection
         }
-    if(playerSelection == computerSelection) return "Tie!"
+    if(playerSelection == computerSelection) return "You Tie! " + playerSelection
 
     return "You Lose! " + computerSelection + " beats " + playerSelection
-  }
+}
+
+function game() {
+    rockBtn.addEventListener('click', () => handleClick('Rock'))
+    paperBtn.addEventListener('click', () => handleClick('Paper'))
+    scissorsBtn.addEventListener('click', () => handleClick('Scissors'))
+}
